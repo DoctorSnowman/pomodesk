c969ff9b93d0ab6014363f9429b761a9653060c1
Ammon <ammon.satterfield@mutualofomaha.com>
2020-10-05 14:46:08 -0500

    Add session pause and cancel capabilities. Refactor time tracking.

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ npm install
 # Run the app
 npm start
 ```
+## Controls
+`p` : pause session  
+`d` : cancel session
 
 ## Build App Executable
 Start terminal in project root directory.
@@ -18,9 +21,13 @@ App may be found in: [pomodesk directory](./pomodesk-darwin-x64)
 
 ## Future Feature Plans
 - option to mark session "success" or "failure" after session
-    - failure count in red, success count in blue/green 
+    - failure count in red, success count in blue/green
+    - possibly track by 0-10 continuum instead
 - option to display counter
-- pause/unpause with `spacebar`
-- quit current countdown with `esc`
+- show options on `esc`
+    - counter display: always, between sessions, never
+    - key mapping
+    - background color selection
+    - start and end messages
 - play boxing bell audio clip (or something motivational) briefly at start
 - add icon to replace default 'electron' icon
--- timer.js
+++ timer.js
@@ -1,52 +1,108 @@
 let victoryClip = new Audio('./resources/FF7 AC Victory Fanfare Ringtone.mp3');
 
-let durationInMinutes = 25;
+// configuration options
+const durationInMinutes = 25;
+const durationInMs = durationInMinutes * 60000;
+const isTimeHidden = false;
+const pausePeriod = 800;
+const startMessageDuration = 2 * 1000;
+
+// init
 let sessionCount = 0;
 let pressedKeys = {};
 let isTimeDisplayDirty = true;
-let end, x;
+let isPaused = false;
+let isSessionRunning = false;
+let elapsed, previousTime, asyncTimerRef, pauseDuration;
 
+// element references
 let startButton = document.getElementById("start-button");
 let timeDisplay = document.getElementById("time-display");
 let counter = document.getElementById("counter");
 let body = document.getElementsByTagName("body")[0];
+
+// eventing
 startButton.addEventListener("click", startTimer);
+window.onkeyup = function(e) { pressedKeys[e.key] = false; };
+window.onkeydown = function(e) { pressedKeys[e.key] = true; };
+window.onkeypress = reactToKeys;
 
-window.onkeyup = function(e) { pressedKeys[e.key] = false; }
-window.onkeydown = function(e) { pressedKeys[e.key] = true; }
-
-function timerLoop () {
-
-  let now = new Date().getTime();
-  let remaining = end - now;
-
-  let minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((remaining % (1000 * 60)) / 1000);
-  if (seconds < 10) seconds = `0${seconds}`;
-
-  if (pressedKeys[" "]) {
-    timeDisplay.innerHTML = minutes + ":" + seconds;
-    isTimeDisplayDirty = true;
-  } else if (isTimeDisplayDirty) {
-    timeDisplay.innerHTML = "In Progress";
-    isTimeDisplayDirty = false;
-  }
-
-  if (remaining < 0) {
-    clearInterval(x);
-    timeDisplay.innerHTML = "VICTORY!";
-    victoryClip.play();
-    startButton.style.display = 'block';
-    sessionCount++;
-    counter.innerHTML = sessionCount.toString();
-    body.style.backgroundColor = "white";
+function reactToKeys(event) {
+  if (isSessionRunning) {
+    switch (event.key) {
+      case 'p':
+        onPausePress();
+        break;
+      case 'd':
+        cancelSession();
+        break;
+      default:
+        break;
+    }
   }
 }
 
+function onPausePress () {
+  isPaused = !isPaused;
+
+  if (isPaused) {
+    timeDisplay.innerHTML = "~ Paused ~";
+    pauseDuration = 0;
+  } else {
+    previousTime = new Date().getTime();
+    timeDisplay.style.opacity = '1'.toString();
+  }
+}
+
+function displayTimer() {
+  if (!isTimeHidden || pressedKeys[" "]) {
+    let remaining = durationInMs - elapsed;
+    if (remaining < 0) remaining = 0;
+    let minutes = Math.floor((remaining % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((remaining % (1000 * 60)) / 1000);
+    if (seconds < 10) seconds = `0${seconds}`;
+    timeDisplay.innerHTML = minutes + ":" + seconds;
+    isTimeDisplayDirty = true;
+  } else if (isTimeHidden && isTimeDisplayDirty) {
+    timeDisplay.innerHTML = "In Progress";
+    isTimeDisplayDirty = false;
+  }
+}
+
+function timerLoop () {
+  let currentTime = new Date().getTime();
+  let delta = currentTime - previousTime;
+  previousTime = currentTime;
+
+  if (isPaused) {
+    pauseDuration += delta;
+    timeDisplay.style.opacity = lerp(pauseDuration);
+  } else {
+    elapsed += delta;
+    if (elapsed > startMessageDuration) {
+      displayTimer();
+      if (elapsed >= durationInMs) {
+        completeSession();
+      }
+    }
+  }
+}
+
+function lerp (delta) {
+  let theta = delta / pausePeriod;
+  let opacity = Math.sin(theta);
+  let smoothOpacity = smooth(opacity);
+  return smoothOpacity.toString();
+}
+
+function smooth(t) {
+  return t*t * (3 - 2*t)
+}
 
 function startTimer () {
-  end = new Date().getTime() + (durationInMinutes * 60000);
-  x = setInterval(timerLoop, 1000);
+  elapsed = 0;
+  previousTime = new Date().getTime();
+  asyncTimerRef = setInterval(timerLoop, 1000 / 24);
 
   startButton.style.display = 'none';
   startButton.innerHTML = 'One More!';
@@ -54,5 +110,27 @@ function startTimer () {
   timeDisplay.style.display = 'block';
   body.style.backgroundColor = "lightskyblue";
   isTimeDisplayDirty = true;
+  isSessionRunning = true;
 }
 
+function cancelSession () {
+  timeDisplay.style.display = 'none';
+  timeDisplay.style.opacity = '1';
+  isPaused = false;
+  resetElements();
+}
+
+function completeSession() {
+  timeDisplay.innerHTML = "VICTORY!";
+  victoryClip.play();
+  sessionCount++;
+  counter.innerHTML = sessionCount.toString();
+  resetElements();
+}
+
+function resetElements () {
+  clearInterval(asyncTimerRef);
+  startButton.style.display = 'block';
+  body.style.backgroundColor = "white";
+  isSessionRunning = false;
+}
